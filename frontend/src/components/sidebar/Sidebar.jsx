53850f700381cf1d9762675b826708042a044bd9
Talentedfalcon <82446314+Talentedfalcon@users.noreply.github.com>
2025-03-30 14:39:42 +0530

    slight change to background color

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import Logoutbutton from "./Logoutbutton";
 
 const Sidebar = () => {
   return (
-    <div className="border-r border-slate-500 p-4 flex flex-col h-full">
+    <div className="border-r border-slate-500 p-4 flex flex-col h-full bg-[#FFFFFF3F]">
       <Sidebarinput />
       <div className="divider px-3"></div>
       <Conversation />
